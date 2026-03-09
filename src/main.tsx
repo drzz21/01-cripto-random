059f74d46363b89074fc016fdb398d4a33e4172c
Francisco Diosdado <drz.y2k@gmail.com>
2026-03-09 14:31:28 -0600

    Tanstack Query Instalación Instalamos React Query de tanstack, las devtools y el linter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+// para habilitar tanstack query
+//debemos habilitar acceso global a toda nuestra app a nuestro query client
+//haciendolo de esta forma, importamos el client y el provider
+//y en este caso tambien las devtools que tambien instalamos
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+//inicializamos el queryclient
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		{/* proveemos el queryclient a toda nuestra app, envolviendo el componente app con el provider y pasandole el client como prop y con
+    esto lo podemos usar dentro de nuestro proyecto */}
+		<QueryClientProvider client={queryClient}>
+			<App />
+			{/* en este caso para las devtools no es necesario pasarle el client, ya que se conecta automaticamente al queryclient que le proveemos a toda nuestra app, por lo que solo lo importamos y lo usamos  */}
+			<ReactQueryDevtools />
+		</QueryClientProvider>
+	</StrictMode>,
+);
+
